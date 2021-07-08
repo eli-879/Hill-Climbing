56dbc70963492cfe9c1f7b7b9058a8fb0ad9abd6
Eric Li <ericxli2001@gmail.com>
2021-07-08 22:31:05 +1200

    improve file

--- main.cpp
+++ main.cpp
@@ -7,15 +7,23 @@
 using namespace std;
 using namespace sf;
 
+int getIndex(string word, char c);
+
 int main() {
 	string path = "aruler.jpeg";
+
+	int index = getIndex(path, '.');
+	
+	string substring = path.substr(0, index);
+
+	cout << substring << endl;
 
 	string filename;
 	string jpg = ".jpg";
 	string jpeg = ".jpeg";
 	string png = ".png";
 
-	int increment = 50;
+	int increment = 25;
 
 	const int height = 800;
 	const int width = 800;
@@ -30,9 +38,21 @@ int main() {
 		int num = picture.ellipse_number();
 		if (num % increment == 0)
 		{
-			filename = "Incremental Pictures/aruler" + std::to_string(num / increment) + jpeg;
+			filename = "Incremental Pictures/a" + substring + std::to_string(num / increment) + jpeg;
+			cout << filename << endl;
 			picture.save_picture(filename);
 		}
 	}
 	return 0;
+}
+
+int getIndex(string word, char c)
+{
+	int length = word.length();
+	for (int i = 0; i < length; i++)
+	{
+		if (word[i] == c) {
+			return i;
+		}
+	}
 }
